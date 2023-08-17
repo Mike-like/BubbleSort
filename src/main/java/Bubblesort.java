026eac010df1c4573375fc1abbb5eb0cecd7fe5d
mike-like <m.bockel@mind-forge.at>
2023-08-17 14:55:23 +0200

    Initial commit

---
+++ src/main/java/Bubblesort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Bubblesort {
+    static void bubbleSort(int[] arr) {
+        System.out.println("sorting....");
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    System.out.println(Arrays.toString(arr));
+                }
+            }
+
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] ={8, 1, 3, 2, 5, 4, 7, 6};
+
+        System.out.println("Array before Bubble Sort");
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+
+        bubbleSort(arr);
+        System.out.println();
+        System.out.println("Array after Bubble Sort");
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
